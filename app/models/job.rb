92d3f000cb3bc47d4d3c7784fc13b6d6ed9af0a8
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-31 11:37:46 +0100

    add notes relationship to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :interviews, dependent: :destroy
+  has_many :notes, dependent: :destroy
   validates :title, :company, presence: true
   validates :title, :company, length: {maximum: 255}
 end
