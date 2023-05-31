42ec51dac23e84a53a18495c2617923724870eaa
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-31 11:36:43 +0100

    add notes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_102703) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_103605) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -51,6 +51,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_102703) do
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "job_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_notes_on_job_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -75,4 +84,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_102703) do
   add_foreign_key "contacts", "users"
   add_foreign_key "interviews", "jobs"
   add_foreign_key "jobs", "users"
+  add_foreign_key "notes", "jobs"
 end
