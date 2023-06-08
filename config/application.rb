a7316a709cdcf8e07d04ee18882f6618f3002fcb
Maxim Prestwich <maximprestwich1@gmail.com>
2023-06-08 14:33:01 +0100

    add secret key base to application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module JobTrackerBack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
+
 
     # Configuration for the application, engines, and railties goes here.
     #
